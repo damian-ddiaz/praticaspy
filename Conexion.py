87b5352059ab48ea2625459b5cd56123f2eab014
Damian Diaz <damiandiaz841@gmail.com>
2024-10-29 23:08:36 -0400

    segundo cambio

---
+++ Conexion.py
@@ -0,0 +1,10 @@
+#pip install mysql-connector-python
+import mysql.connector
+
+class Cconexion:
+    def ConexionBaseDeDatos():
+        try:
+            conexion = mysql.connector.connect(user=' ',password='',host='',database='',port='')
+            
+        except mysql.connector.Error as error:
+            print("Error al Conectarse a la bas de Datos {}".format(error))
