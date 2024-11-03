4a46956f9267e4026205b487bd7a85136fc732d2
Damian Diaz <damiandiaz841@gmail.com>
2024-11-02 22:12:07 -0400

    cuarto cambio cambio

---
+++ Clientes.py
@@ -0,0 +1,21 @@
+from Conexion import *
+
+class CClientes:
+    def ingresarClientes(nombre,correo,clave,estatus):
+        try:
+            cone = CConexion.ConexionBaseDeDatos()
+            cursor = cone.cursor() 
+            sql = "insert into usuarios valus (null,%s,%s,%s,%s);"
+            #La Variable Valores tiene que ser una tupla
+            #Como la minima expresion es: (valos,) la coma hace que se una tupla
+            # las tuplas son listas inmutables, eso quiere decir que no se pueden modificar
+            
+            valores = (nombre,correo,clave,estatus)
+            cursor.execute(sql.valores)
+            cone.commit()
+            
+            print(cursor.rowcount,"Registro Ingresado")
+            cone.close()
+            
+        except mysql.connector.Error as error:
+            print("Error de Ingreso de datos {}".format(error))
--- Conexion.py
+++ Conexion.py
@@ -1,10 +1,15 @@
 #pip install mysql-connector-python mm
 import mysql.connector
 
-class Cconexion:
+class CConexion:
     def ConexionBaseDeDatos():
         try:
-            conexion = mysql.connector.connect(user=' ',password='',host='',database='',port='')
+            conexion = mysql.connector.connect(user='root',password='Gemelas2000#',
+                                               host='127.0.0.1',database='dbsaemp',port='3306')
             
+            print("Conexion Correcta")
+            return conexion
         except mysql.connector.Error as error:
             print("Error al Conectarse a la bas de Datos {}".format(error))
+            
+    ConexionBaseDeDatos()
