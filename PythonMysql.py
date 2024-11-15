ab8475773849bf33cd56484ae4839d2167b862a4
D & D sistemas <damiandiaz841@gmail.com>
2024-11-15 11:39:28 -0400

    Altualizacion 15/111/2024

--- PythonMysql.py
+++ PythonMysql.py
@@ -8,31 +8,44 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 
+from Usuarios import *
+from Conexion import * 
+
 class FormularioUsarios:
-    def Formulario():
+
+    global base
+    base = None
+
+    global texBoxId
+    texBoxId = None
+
+    global texBoxNombre
+    texBoxNombre = None
+
+    global texBoxCorreo
+    texBoxCorreo = None
+
+    global texBoxClave
+    texBoxClave = None
+
+    global seleecionEtatus
+    seleecionEtatus = None
+
+    global groupBox
+    groupBox = None
+
+    global tree
+    tree = None
+     
+def Formulario():
         global base
-        base = None
-
         global texBoxId
-        texBoxId = None
-
         global texBoxNombre
-        texBoxNombre = None
-
         global texBoxCorreo
-        texBoxCorreo = None
-
         global texBoxClave
-        texBoxClave = None
-
         global seleecionEtatus
-        seleecionEtatus = None
-
         global groupBox
-        groupBox = None
-
         global tree
-        tree = None
     
         try:
             base =Tk()
@@ -47,50 +60,79 @@ class FormularioUsarios:
             texBoxId.grid(row=0,column=1)
              
             labelNombre = Label(groupBox,text="Nombres",width=13,font=("arial",12)).grid(row=1,column=0)
-            texBoxId = Entry(groupBox)
-            texBoxId.grid(row=1,column=1)
+            texBoxNombre = Entry(groupBox)
+            texBoxNombre.grid(row=1,column=1)
              
             labelCorreo = Label(groupBox,text="Correo",width=13,font=("arial",12)).grid(row=2,column=0)
-            texBoxId = Entry(groupBox)
-            texBoxId.grid(row=2,column=1) 
+            texBoxCorreo = Entry(groupBox)
+            texBoxCorreo.grid(row=2,column=1) 
             
             labelClave = Label(groupBox,text="Clave",width=13,font=("arial",12)).grid(row=3,column=0)
-            texBoxId = Entry(groupBox)
-            texBoxId.grid(row=3,column=1) 
+            texBoxClave = Entry(groupBox)
+            texBoxClave.grid(row=3,column=1) 
             
             labelEstatus = Label(groupBox,text="Estatus",width=13,font=("arial",12)).grid(row=4,column=0)
             seleecionEtatus = tk.StringVar()
             combo = ttk.Combobox(groupBox,values=["Activo","Inactivo"],textvariable=seleecionEtatus)
             combo.grid(row=4,column=1)  
-            seleecionEtatus.set("Activo")        
-                   
-                
+            seleecionEtatus.set("Activo")       
+
+            Button(groupBox,text="Guardar",width=10,command=guardarRegistros).grid(row=5,column=0) 
+            Button(groupBox,text="Modificar",width=10).grid(row=5,column=1) 
+            Button(groupBox,text="Eliminar",width=10).grid(row=5,column=2) 
+
+            groupBox = LabelFrame(base,text="Lista de Personal",padx=5,pady=5,)
+            groupBox.grid(row=0,column=1,padx=5,pady=5)
+            
+            # Crear un TREVIEW
+            tree = ttk.Treeview(groupBox,columns=("id","Nombre","Correo","contraseña","Estatus"),show='headings',height=5,)
+            tree.column("# 1",anchor=CENTER)
+            tree.heading("# 1", text="Id")
+            tree.column("# 2",anchor=CENTER)
+            tree.heading("# 2", text="Nombre")
+            tree.column("# 3",anchor=CENTER)
+            tree.heading("# 3", text="Correo")
+            tree.column("# 4",anchor=CENTER)
+            tree.heading("# 4", text="Clave")
+            tree.column("# 3",anchor=CENTER)
+            tree.heading("# 3", text="Estaus")
+
+            #Agregar los Datos a la Tabla
+            # Mostrar la Tabla
+            for row in CUsuarios.mostrarUsuarios():
+                tree.insert("","end",values=row)
+
+            tree.pack()
+
             base.mainloop()
         except ValueError as error:
             print("Error al mostrar la interfaz, error: {}".format(error))
-
-    def guardarRegistros():
-
-        global texBoxNombre, texBoxCorreo, texBoxClave, seleecionEtatus
+    
+def guardarRegistros():
+        global texBoxNombre,texBoxCorreo,texBoxClave,seleecionEtatus
 
         try:
+            #verificar si los widgets estanm inicializados
             if texBoxNombre is None or texBoxCorreo is None or texBoxClave is None or seleecionEtatus is None:
-                print("Los campos no estan Inicializados")
+                print("Los widgets No estan Inicializados")
+                return
 
             nombre  = texBoxNombre.get()
-            correo  = texBoxCorreo.get()
+            correo  = texBoxCorreo.get() 
             clave   = texBoxClave.get()
-            estatus = seleecionEtatus.get()
+            estatus  = seleecionEtatus.get()
 
             CUsuarios.ingresarUsuarios(nombre,correo,clave,estatus)
-            messagebox.showinfo("Informacion","Los datos fueron guardados")
+            messagebox.showinfo("Informcion","Los Datos Fueron Guardados")
 
-            #limpiamos los campos
+            #limiando Campos
             texBoxNombre.delete(0,END)
             texBoxCorreo.delete(0,END)
             texBoxClave.delete(0,END)
-        
+
+  
         except ValueError as error:
-            print("Erroro al Ingresara datos {}".format(error))
-                
-    Formulario()
+            print("Error el ingresar datos {}".format(error))
+
+
+Formulario()
--- Usuarios.py
+++ Usuarios.py
@@ -1,17 +1,31 @@
 from Conexion import *
 
 class CUsuarios:
+    def mostrarUsuarios():
+        try:
+            cone = CConexion.ConexionBaseDeDatos()
+            cursor = cone.cursor() 
+            cursor.execute ("select * from usuario;")
+            miResultado = cursor.fetchall()
+            cone.commit()
+            cone.close()
+            return miResultado
+
+        except mysql.connector.Error as error:
+            print("Error al Mostrar datos {}".format(error))
+
+
     def ingresarUsuarios(nombre,correo,clave,estatus):
         try:
             cone = CConexion.ConexionBaseDeDatos()
             cursor = cone.cursor() 
-            sql = "insert into usuarios valus (null,%s,%s,%s,%s);"
+            sql = "insert into usuario values(null,%s,%s,%s,%s);"
             #La Variable Valores tiene que ser una tupla
             #Como la minima expresion es: (valos,) la coma hace que se una tupla
             # las tuplas son listas inmutables, eso quiere decir que no se pueden modificar
             
             valores = (nombre,correo,clave,estatus)
-            cursor.execute(sql.valores)
+            cursor.execute(sql,valores)
             cone.commit()
             
             print(cursor.rowcount,"Registro Ingresado")
